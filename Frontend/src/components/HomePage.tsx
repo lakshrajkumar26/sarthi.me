dc290a34ae803912b61eda53c0a6f655780db9d2
lakshrajkumar26 <lakshrajkumar.791.lrk@gmail.com>
2025-07-08 11:14:21 +0530

    added confidence

--- Frontend/src/components/HomePage.tsx
+++ Frontend/src/components/HomePage.tsx
@@ -11,20 +11,28 @@ const HomePage = () => {
  const [loading ,setLoading] = useState<boolean>(false);
  
  //emotion from backend
- const [emotion , setEmotion] = useState<string>("");
+ const [emotion , setEmotion] = useState<string | null > ("");
+ const [confidence ,setConfidence] = useState<number | null>(0);
 
 
 const handleSubmit = async () => {
+    const URL ="";
     setLoading(true); //before async starts
+    setEmotion(null);
+    setConfidence(null);
+
     try{
-    const URL =""
     console.log(text);
     const res = await axios.post(URL,{text});
     console.log ("res send",res.data)
+    setEmotion(res.data.emotion);
+    setConfidence(res.data.confidence);
     
     }
     catch(err){
-        console.log("error occurred in posting data",err)
+        console.log("error occurred in posting data",err);
+        setEmotion('Error fetching emotion');
+        setConfidence(null);
     }finally   // after async completes
     {
        setLoading(false);
@@ -33,8 +41,13 @@ const handleSubmit = async () => {
 }
   return (
     <><div className="main"> 
-    <div><h1>Emotion Reflection Tool</h1> </div>
-    <textarea name="" id="" placeholder='Enter Your Text...' onChange={(e : React.ChangeEvent<HTMLTextAreaElement>)=>(setText(e.target.value))}></textarea>
+    <h1>Emotion Reflection Tool</h1> 
+
+    <textarea
+     placeholder='Enter Your Text...'
+     onChange={(e : React.ChangeEvent<HTMLTextAreaElement>)=>(setText(e.target.value))}
+     value={text}/>
+
     <button  onClick={()=>handleSubmit()} disabled={loading} >{loading ?  'Submitting...' : 'Submit'}</button>
     <div className="display-emotion">
       <h2>Emotion {}</h2>
